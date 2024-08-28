fe5ae903027468db53d4cd8d3b06d9a1777dd599
Kashish pratap Singh <singhkashishpratap@gmail.com>
2024-08-29 00:03:30 +0530

    Happy file  in git course

---
+++ Happy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Happy {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        String s=scn.next();
+        System.out.println(s);
+    }
+}
