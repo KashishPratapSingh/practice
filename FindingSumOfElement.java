11150625046116e04ebff0f952ced0d779db3cbd
Kashish Pratap Singh <singhkashishpratap@gmail.com>
2024-11-04 21:41:59 +0530

    Finding sum of element in an array

---
+++ FindingSumOfElement.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FindingSumOfElement {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        System.out.println(findingSumOfElement(arr));
+    }
+    public static int findingSumOfElement(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
