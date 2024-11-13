2c27512feb461b95c778b53f7b89460d02519e09
Kashish Pratap Singh <singhkashishpratap@gmail.com>
2024-11-13 21:41:07 +0530

    Sum of Sqaure of digit

---
+++ SumOfDigit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        System.out.println(sumOfDigit(n));
+    }
+
+    public static int sumOfDigit(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int d = n % 10;
+            sum += (int) Math.pow(d, 2);
+            n /= 10;
+        }
+        return sum;
+    }
+}
